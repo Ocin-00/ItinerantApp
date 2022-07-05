a526b7373fa013981ffa271afe048d1f7fbb04e3
NICOLÁS ALCANTARILLA CHAVES <nialcha@upv.edu.es>
2022-07-05 15:55:00 +0200

    Añadidas las entidades (modelos) del programa

--- test/DbTest.java
+++ test/DbTest.java
@@ -2,17 +2,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import com.itinerant.entity.LocalidadAvant;
+import com.itinerant.entity.Localidad;
 
 public class DbTest {
 
 	public static void main(String[] args) {
-		LocalidadAvant pueblo = new LocalidadAvant();
-		pueblo.setCodPostal(46300);
-		pueblo.setNombre("Utiel");
-		pueblo.setComarca("Plana de Utiel-Requena");
-		pueblo.setPoblacion(11000);
-		pueblo.setMancomunidad("Mancomunidad del interior - Tierra del vino");
+		Localidad pueblo = new Localidad(46330, "Camporrobles", "Plana de Utiel-Requena", 1200/*, 
+				"Mancomunidad del interior - Tierra del vino", null*/);
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("ItinerantApp");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		
